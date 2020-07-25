d6a41e2eaaa25fcc87343674cfbfddf097b50bd8
Gavin <gavinbrrtt@gmail.com>
2020-07-25 01:37:57 -0700

    Made a simple skeleton of the UI and added a dropzone component for file uploads.

--- modules/exif.py
+++ modules/exif.py
@@ -1,3 +1,12 @@
-def read_exif():
-	print('hello')
+from io import BytesIO
+from PIL import Image, ExifTags
 
+def read_exif(img_data):
+	# open the image
+	image = Image.open(BytesIO(img_data.read()))
+	# return the exif data
+	ex_data = image._getexif()
+	collection = {}
+	for key, value in ex_data.items():
+		collection[ExifTags.TAGS.get(key)] = value
+	return collection
--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,69 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+import Dropzone from 'react-dropzone';
 
-function App() {
-
+function FileUploader() {
+	// create a hook for the file path
 	const [path, updatePath] = useState('');
 
-	let uploadFile = async () => {
+	const uploadFile = async () => {
+		// add the selected file to a form
 		let formData = new FormData();
 		formData.append('file', path);
-		console.log(path);
+		// retrieve exif metadata
 		const resp = await fetch('/upload', { method: 'POST', body: formData });
-		let j = await resp.json();
-		console.log(j);
+		console.log(resp);
 	}
 
-	let update = async (event) => {
+	const update = async event => {
+		// update the file path
 		await updatePath(event.target.files[0]);
 	}
 
-	return (<div>
-	<input type='file' accept='image/*' onChange={(e) => update(e)}/>
-	<button type='submit' onClick={() => uploadFile()}>Upload</button>
+	const dropFile = async event => {
+		alert(event.target.files[0]);
+	}
+
+	return (<div id='fileUploader'>
+	{/*<input type='file' accept='image/*' onChange={(e) => update(e)}/>*/}
+	{/*<button id='upload' type='submit' onClick={() => uploadFile()}>Upload</button>*/}
+	<Dropzone id='dropzone' accept='image/*' onDrop={file => dropFile(file)}>
+	 		{({getRootProps, getInputProps, isDragActive, isDragReject, acceptedFiles}) => (
+    	<div id="dropper" {...getRootProps()}>
+      	<input type="file" {...getInputProps()} />
+      	{!isDragActive && acceptedFiles.length == 0 && "Click here or drag a file to upload!"}
+		{isDragActive && !isDragReject && "Drop your file here!"}
+		{isDragActive && isDragReject && "Please enter an image file"}
+		{acceptedFiles.length > 0 && !isDragActive && !isDragReject && acceptedFiles[0].name}
+		</div>
+  	)}
+	</Dropzone>
+	</div>);
+}
+
+function ExifViewer() {
+	return (<div id='exifViewer'>
+		<p>ExifViewer</p>
+	</div>);
+}
+
+function Page() {
+	return (<div id='page'>
+	<FileUploader/>
+	<ExifViewer/>
+	</div>);
+}
+
+function Heading(props) {
+	return(<div id='heading'>
+		{props.title}
+	</div>);
+}
+
+function App() {
+	return (<div id='app'>
+		<Heading title={'exif god'}/>
+		<Page/>
 	</div>);
 }
 
